0efed797e795979208dd56662b63c0ff4c57700b
RITHIKA V <rithikakalai927@gmail.com>
2026-09-04 14:03:15 +0530

    Update Program.cs

--- CommBank-Server/Program.cs
+++ CommBank-Server/Program.cs
@@ -11,8 +11,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Secrets.json");
 
-var mongoClient = new MongoClient(builder.Configuration.GetConnectionString("CommBank"));
-var mongoDatabase = mongoClient.GetDatabase("CommBank");
+var mongoClient = new MongoClient(builder.Configuration.GetConnectionString("commbank"));
+var mongoDatabase = mongoClient.GetDatabase("commbank");
 
 IAccountsService accountsService = new AccountsService(mongoDatabase);
 IAuthService authService = new AuthService(mongoDatabase);
